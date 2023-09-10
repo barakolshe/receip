6aac22ba0a9b792096cdb29481d5237ab9ee1b8a
barakolshe <barakolshe@gmail.com>
2023-09-10 13:15:41 +0000

    made interpreter methods static

--- interpreter/image_manager.py
+++ interpreter/image_manager.py
@@ -1,5 +1,6 @@
 from PIL import Image as PilImage
 
 class Image:
+    @staticmethod
     def open(path):
         return PilImage.open(path)
--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,6 +2,6 @@ import pytesseract
 from image_manager import Image
 
 class Interpreter:
-    def interpret(path):
+    def interpret(self, path):
         text = pytesseract.image_to_string(Image.open(path), lang="ocr+eng")
         print(text)  
